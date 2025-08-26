997a9550a09ef16976f1a728b0fbab683bd5d6f6
jcolange <jade.colanges@live.fr>
2025-08-26 12:56:13 +0200

    fixed the returns to return nil in case of error

--- utils/filemaker.go
+++ utils/filemaker.go
@@ -11,18 +11,18 @@ func FileMaker(args []string) *os.File {
 
 	if len(args) != 2 {
 		fmt.Println("ERROR : Program needs two arguments to run")
-		return content
+		return nil
 	}
 	file := os.Args[1]
 
 	content, err := os.Open("./examples/" + file)
 	if err != nil {
 		fmt.Println("ERROR : Error opening file ", err)
-		return content
+		return nil
 	}
 
 	if !checks.FirstCheck(content) {
-		return content
+		return nil
 	}
 
 	return content
