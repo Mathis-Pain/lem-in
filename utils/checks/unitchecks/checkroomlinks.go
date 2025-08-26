87b0ea5827d2f49188be60734ef4b4e3eb28c5d6
jcolange <jade.colanges@live.fr>
2025-08-26 17:58:58 +0200

    simplified code and added comments

--- utils/checks/unitchecks/checkroomlinks.go
+++ utils/checks/unitchecks/checkroomlinks.go
@@ -6,21 +6,16 @@ import (
 )
 
 func CheckRoomLinks(AllRooms models.Roomlist, Links []models.Link) bool {
-	var names []string
-
-	names = append(names, AllRooms.End.Name)
-	names = append(names, AllRooms.Start.Name)
-
-	for _, roomName := range AllRooms.Rooms {
-		names = append(names, roomName.Name)
-	}
-
+	// Récupère toutes les salles existant dans le fichier
 	validName := make(map[string]bool)
 
-	for _, v := range names {
-		validName[v] = true
+	validName[AllRooms.End.Name] = true
+	validName[AllRooms.Start.Name] = true
+	for _, roomName := range AllRooms.Rooms {
+		validName[roomName.Name] = true
 	}
 
+	// Vérifie pour chaque Link si la salle "From" et la salle "To" existent bien dans la liste des salles
 	for _, current := range Links {
 		if !validName[current.To] {
 			fmt.Println("ERROR : Wrong format. One or more links contain invalid room name : ", current.To)
