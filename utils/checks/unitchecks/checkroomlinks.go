2d19524a050fd2ab16de99974e10f6763f2ec7de
mpain <mathis.pain76@gmail.com>
2025-08-26 17:25:15 +0200

    rebase jcolange

---
+++ utils/checks/unitchecks/checkroomlinks.go
@@ -0,0 +1,36 @@
+package unitchecks
+
+import (
+	"fmt"
+	"lem-in/models"
+)
+
+func CheckRoomLinks(AllRooms models.Roomlist, Links []models.Link) bool {
+	var names []string
+
+	names = append(names, AllRooms.End.Name)
+	names = append(names, AllRooms.Start.Name)
+
+	for _, roomName := range AllRooms.Rooms {
+		names = append(names, roomName.Name)
+	}
+
+	validName := make(map[string]bool)
+
+	for _, v := range names {
+		validName[v] = true
+	}
+
+	for _, current := range Links {
+		if !validName[current.To] {
+			fmt.Println("ERROR : Wrong format. One or more links contain invalid room name : ", current.To)
+			return false
+		} else if !validName[current.From] {
+			fmt.Println("ERROR : Wrong format. One or more links contain invalid room name : ", current.From)
+			return false
+		}
+
+	}
+
+	return true
+}
